9a1533e9d11c54364be780c0bed1c4dd4eeaf58c
rubyghabboun <139931157+rubyghabboun@users.noreply.github.com>
2023-12-11 21:54:18 -0500

    Update crowdfunding_db_schema.sql

--- quickDBD_files/crowdfunding_db_schema.sql
+++ quickDBD_files/crowdfunding_db_schema.sql
@@ -5,25 +5,25 @@
 CREATE TABLE "Campaign" (
     "cf_id" int   NOT NULL,
     "contact_id" int   NOT NULL,
-    "company_name" varchar(255)   NOT NULL,
+    "company_name" varchar(255)  NOT NULL,
     "description" varchar(255)   NOT NULL,
     "goal" numeric   NOT NULL,
     "pledged" numeric   NOT NULL,
     "outcome" varchar(255)   NOT NULL,
     "backers_count" int   NOT NULL,
-    "country" varchar(255)   NOT NULL,
+    "country" varchar(255)  NOT NULL,
     "currency" varchar(255)   NOT NULL,
     "launch_date" date   NOT NULL,
     "end_date" date   NOT NULL,
-    "category_id" varchar(255   NOT NULL,
-    "subcategory_id" string   NOT NULL,
+    "category_id" varchar(255)   NOT NULL,
+    "subcategory_id" varchar(255)  NOT NULL,
     CONSTRAINT "pk_Campaign" PRIMARY KEY (
         "cf_id"
      )
 );
 
 CREATE TABLE "Category" (
-    "category_id" varchar(255)   NOT NULL,
+    "category_id" varchar(255)  NOT NULL,
     "Category" varchar(255)   NOT NULL,
     CONSTRAINT "pk_Category" PRIMARY KEY (
         "category_id"
@@ -34,15 +34,15 @@ CREATE TABLE "Contacts" (
     "contact_id" int   NOT NULL,
     "first_name" varchar(255)   NOT NULL,
     "last_name" varchar(255)   NOT NULL,
-    "email" varchar(255)   NOT NULL,
+    "email" varchar(255)  NOT NULL,
     CONSTRAINT "pk_Contacts" PRIMARY KEY (
         "contact_id"
      )
 );
 
 CREATE TABLE "Subcategory" (
-    "subcategory_id" varchar(255)   NOT NULL,
-    "subcategory" varchar(255)   NOT NULL,
+    "subcategory_id" varchar(255)  NOT NULL,
+    "subcategory" varchar(255)  NOT NULL,
     CONSTRAINT "pk_Subcategory" PRIMARY KEY (
         "subcategory_id"
      )
@@ -56,4 +56,3 @@ REFERENCES "Category" ("category_id");
 
 ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "Subcategory" ("subcategory_id");
-
